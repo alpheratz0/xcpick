078077b267b38bd92d0ac9a05af5e0eb5df73e03
alpheratz0 <alpheratz99@protonmail.com>
2022-06-25 15:50:29 -0300

    refactor: small changes

--- Makefile
+++ Makefile
@@ -18,28 +18,28 @@ ${OBJ}:	src/debug.h \
 		src/cursorfont.h
 
 xcpick: ${OBJ}
-	@${CC} -o $@ ${OBJ} ${LDFLAGS}
+	${CC} -o $@ ${OBJ} ${LDFLAGS}
 
 install: all
-	@mkdir -p ${DESTDIR}${PREFIX}/bin
-	@cp -f xcpick ${DESTDIR}${PREFIX}/bin
-	@chmod 755 ${DESTDIR}${PREFIX}/bin/xcpick
-	@mkdir -p ${DESTDIR}${MANPREFIX}/man1
-	@cp -f man/xcpick.1 ${DESTDIR}${MANPREFIX}/man1
-	@chmod 644 ${DESTDIR}${MANPREFIX}/man1/xcpick.1
+	mkdir -p ${DESTDIR}${PREFIX}/bin
+	cp -f xcpick ${DESTDIR}${PREFIX}/bin
+	chmod 755 ${DESTDIR}${PREFIX}/bin/xcpick
+	mkdir -p ${DESTDIR}${MANPREFIX}/man1
+	cp -f man/xcpick.1 ${DESTDIR}${MANPREFIX}/man1
+	chmod 644 ${DESTDIR}${MANPREFIX}/man1/xcpick.1
 
 dist: clean
-	@mkdir -p xcpick-${VERSION}
-	@cp -R LICENSE Makefile README man src xcpick-${VERSION}
-	@tar -cf xcpick-${VERSION}.tar xcpick-${VERSION}
-	@gzip xcpick-${VERSION}.tar
-	@rm -rf xcpick-${VERSION}
+	mkdir -p xcpick-${VERSION}
+	cp -R LICENSE Makefile README man src xcpick-${VERSION}
+	tar -cf xcpick-${VERSION}.tar xcpick-${VERSION}
+	gzip xcpick-${VERSION}.tar
+	rm -rf xcpick-${VERSION}
 
 uninstall:
-	@rm -f ${DESTDIR}${PREFIX}/bin/xcpick
-	@rm -f ${DESTDIR}${MANPREFIX}/man1/xcpick.1
+	rm -f ${DESTDIR}${PREFIX}/bin/xcpick
+	rm -f ${DESTDIR}${MANPREFIX}/man1/xcpick.1
 
 clean:
-	@rm -f xcpick xcpick-${VERSION}.tar.gz ${OBJ}
+	rm -f xcpick xcpick-${VERSION}.tar.gz ${OBJ}
 
 .PHONY: all clean install uninstall dist
--- src/xcpick.c
+++ src/xcpick.c
@@ -51,10 +51,7 @@
 #include "debug.h"
 #include "cursorfont.h"
 
-typedef struct point {
-	int16_t x;
-	int16_t y;
-} point_t;
+#define XCB_PLANES_ALL_PLANES ((uint32_t)(~0UL))
 
 static inline void
 print_opt(const char *sh, const char *lo, const char *desc)
@@ -62,11 +59,10 @@ print_opt(const char *sh, const char *lo, const char *desc)
 	printf("%7s | %-25s %s\n", sh, lo, desc);
 }
 
-static bool
+static int
 match_opt(const char *in, const char *sh, const char *lo)
 {
-	return (strcmp(in, sh) == 0) ||
-		   (strcmp(in, lo) == 0);
+	return (strcmp(in, sh) == 0) || (strcmp(in, lo) == 0);
 }
 
 static void
@@ -86,10 +82,10 @@ version(void)
 	exit(0);
 }
 
-static point_t
-xcb_get_pointer_position(xcb_connection_t *connection, xcb_window_t window)
+static xcb_point_t
+get_pointer_position(xcb_connection_t *connection, xcb_window_t window)
 {
-	point_t position;
+	xcb_point_t position;
 	xcb_query_pointer_cookie_t cookie;
 	xcb_query_pointer_reply_t *reply;
 	xcb_generic_error_t *error;
@@ -112,7 +108,7 @@ xcb_get_pointer_position(xcb_connection_t *connection, xcb_window_t window)
 }
 
 static xcb_cursor_t
-xcb_load_cursor(xcb_connection_t *connection, int16_t id)
+load_cursor(xcb_connection_t *connection, int16_t id)
 {
 	xcb_font_t font;
 	xcb_cursor_t cursor;
@@ -147,7 +143,10 @@ xcb_load_cursor(xcb_connection_t *connection, int16_t id)
 }
 
 static uint32_t
-xcb_get_color_at(xcb_connection_t *connection, xcb_window_t window, int16_t x, int16_t y)
+get_color_at(xcb_connection_t *connection,
+             xcb_window_t window,
+             int16_t x,
+             int16_t y)
 {
 	xcb_get_image_reply_t *reply;
 	xcb_get_image_cookie_t cookie;
@@ -159,7 +158,7 @@ xcb_get_color_at(xcb_connection_t *connection, xcb_window_t window, int16_t x, i
 	error = NULL;
 	cookie = xcb_get_image(
 		connection, XCB_IMAGE_FORMAT_Z_PIXMAP,
-		window, x, y, 1, 1, (uint32_t)(~0UL)
+		window, x, y, 1, 1, XCB_PLANES_ALL_PLANES
 	);
 
 	reply = xcb_get_image_reply(connection, cookie, &error);
@@ -196,7 +195,7 @@ main(int argc, char **argv)
 	xcb_motion_notify_event_t *mnev;
 	xcb_button_press_event_t *bpev;
 	uint32_t fill_color, border_color;
-	point_t pointer_position;
+	xcb_point_t pointer_position;
 	bool print_newline;
 	int exit_status;
 
@@ -216,7 +215,7 @@ main(int argc, char **argv)
 		die("can't get default screen");
 	}
 
-	cursor = xcb_load_cursor(connection, XC_GOBBLER);
+	cursor = load_cursor(connection, XC_GOBBLER);
 
 	gpr = xcb_grab_pointer_reply(
 		connection,
@@ -238,8 +237,8 @@ main(int argc, char **argv)
 	window = xcb_generate_id(connection);
 	exit_status = 0;
 	print_newline = isatty(STDOUT_FILENO);
-	pointer_position = xcb_get_pointer_position(connection, screen->root);
-	fill_color = xcb_get_color_at(connection, screen->root, pointer_position.x, pointer_position.y);
+	pointer_position = get_pointer_position(connection, screen->root);
+	fill_color = get_color_at(connection, screen->root, pointer_position.x, pointer_position.y);
 	border_color = 0xffffff;
 
 	xcb_create_window(
@@ -264,61 +263,61 @@ main(int argc, char **argv)
 
 	while ((ev = xcb_wait_for_event(connection))) {
 		switch (ev->response_type & ~0x80) {
-			case XCB_MOTION_NOTIFY:
-				mnev = (xcb_motion_notify_event_t *)(ev);
+		case XCB_MOTION_NOTIFY:
+			mnev = (xcb_motion_notify_event_t *)(ev);
 
-				fill_color = xcb_get_color_at(
-					connection, screen->root,
-					mnev->event_x, mnev->event_y
-				);
+			fill_color = get_color_at(
+				connection, screen->root,
+				mnev->event_x, mnev->event_y
+			);
 
-				xcb_change_window_attributes(
-					connection, window,
-					XCB_CW_BACK_PIXEL, &fill_color
-				);
+			xcb_change_window_attributes(
+				connection, window,
+				XCB_CW_BACK_PIXEL, &fill_color
+			);
 
-				xcb_clear_area(connection, 0, window, 0, 0, 44, 44);
+			xcb_clear_area(connection, 0, window, 0, 0, 44, 44);
 
-				xcb_configure_window(
-					connection, window,
-					XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y,
-					(const uint32_t[2]) {
-						mnev->event_x < 25 ?
-							25 :
-							(mnev->event_x >= screen->width_in_pixels - 75 ?
-								screen->width_in_pixels - 75 :
-								mnev->event_x
-							),
-						mnev->event_y >= screen->height_in_pixels - 75 ?
-							mnev->event_y - 75 :
-							mnev->event_y + 25,
-					}
-				);
-
-				xcb_flush(connection);
-				break;
-			case XCB_BUTTON_PRESS:
-				bpev = (xcb_button_press_event_t *)(ev);
-
-				switch (bpev->detail) {
-					case XCB_BUTTON_INDEX_1:
-						printf("%06x%s", fill_color, print_newline ? "\n" : "");
-						free(ev);
-						goto end;
-						break;
-					case XCB_BUTTON_INDEX_2:
-					case XCB_BUTTON_INDEX_3:
-						exit_status = 2;
-						free(ev);
-						goto end;
-						break;
-					default:
-						break;
+			xcb_configure_window(
+				connection, window,
+				XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y,
+				(const uint32_t[2]) {
+					mnev->event_x < 25 ?
+						25 :
+						(mnev->event_x >= screen->width_in_pixels - 75 ?
+							screen->width_in_pixels - 75 :
+							mnev->event_x
+						),
+					mnev->event_y >= screen->height_in_pixels - 75 ?
+						mnev->event_y - 75 :
+						mnev->event_y + 25,
 				}
+			);
 
-				break;
-			default:
-				break;
+			xcb_flush(connection);
+			break;
+		case XCB_BUTTON_PRESS:
+			bpev = (xcb_button_press_event_t *)(ev);
+
+			switch (bpev->detail) {
+				case XCB_BUTTON_INDEX_1:
+					printf("%06x%s", fill_color, print_newline ? "\n" : "");
+					free(ev);
+					goto end;
+					break;
+				case XCB_BUTTON_INDEX_2:
+				case XCB_BUTTON_INDEX_3:
+					exit_status = 2;
+					free(ev);
+					goto end;
+					break;
+				default:
+					break;
+			}
+
+			break;
+		default:
+			break;
 		}
 
 		free(ev);
